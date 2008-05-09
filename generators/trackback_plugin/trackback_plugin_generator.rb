1721a8ff46d40ad763d35527768ecedf1486ce51
gendosu <gen_takahashi@yahoo.co.jp>
2008-05-09 08:57:12 +0000

    ジェネレータのroutes.rb編集部分を修正(#65)

--- generators/trackback_plugin/trackback_plugin_generator.rb
+++ generators/trackback_plugin/trackback_plugin_generator.rb
@@ -1,7 +1,13 @@
+#require 'delegate'
+#require 'optparse'
+#require 'fileutils'
+#require 'tempfile'
+#require 'erb'
+
 class TrackbackPluginGenerator < Rails::Generator::NamedBase
   
   attr_reader :controller_name, :controller_class_path, :controller_file_path, :controller_class_nesting, :controller_class_nesting_depth,
-    :controller_class_name, :controller_singular_name, :controller_plural_name
+  :controller_class_name, :controller_singular_name, :controller_plural_name
   alias_method  :controller_file_name,  :controller_singular_name
   alias_method  :controller_table_name, :controller_plural_name
   
@@ -33,7 +39,7 @@ class TrackbackPluginGenerator < Rails::Generator::NamedBase
       # コントローラを配置
       m.template 'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
       
-      m.route_resources controller_file_name
+      route_resources controller_file_name
     end 
   end
   
@@ -45,5 +51,23 @@ class TrackbackPluginGenerator < Rails::Generator::NamedBase
   # Override with your own usage banner.
   def banner
       "Usage: #{$0} trackback_plugin ModelName [ControllerName]"
+  end
+  
+  def route_resources(resource)
+    resource = resource.to_sym.inspect
+    sentinel = 'ActionController::Routing::Routes.draw do |map|'
+    
+    logger.route "map.resources #{resource}, :member => {:trackback => :post}"
+    unless options[:pretend]
+      gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+              "#{match}\n  map.resources #{resource}, :member => {:trackback => :post}\n"
+      end
+    end
+  end
+  
+  def gsub_file(relative_destination, regexp, *args, &block)
+    path = destination_path(relative_destination)
+    content = File.read(path).gsub(regexp, *args, &block)
+    File.open(path, 'wb') { |file| file.write(content) }
   end
 end
